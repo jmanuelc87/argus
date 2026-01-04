4ab3602280dde740de032d46323dcfde0d9af6b7
Juan Manuel Carballo <jm.carb@gmail.com>
2026-01-03 19:44:23 -0600

    Updated driver tests for serial and canbus

--- tests/test_canbus_driver.py
+++ tests/test_canbus_driver.py
@@ -1,21 +1,18 @@
 import time
 import logging
 
-from argus.canbus import Driver
+from argus.driver import CanbusDriver
 
 
 log = logging.getLogger(__file__)
 
 
 def test_ping():
-    driver = Driver(
+    driver = CanbusDriver(
         interface="slcan", channel="/dev/tty.usbmodem206B358043331", bitrate=500000
     )
-    driver.ping()
 
-    time.sleep(0.1)
-
-    msg = driver.get_latest_message()
+    msg = driver.ping()
 
     assert msg == "OK!"
 
--- tests/test_serial_driver.py
+++ tests/test_serial_driver.py
@@ -1,93 +1,80 @@
 import time
 import logging
 
-from argus.serial import Driver
+from argus.driver import SerialDriver
 
 
 log = logging.getLogger(__file__)
 
 
 def test_ping():
-    driver = Driver("/dev/tty.usbserial-2130", report=True)
-    driver.ping()
-
-    time.sleep(0.1)
-
-    msg = driver.get_latest_message()
+    driver = SerialDriver("/dev/tty.usbserial-2130", report=True)
+    msg = driver.ping()
 
     assert msg == "OK!"
 
     log.info(msg)
+
+    driver.close()
 
 
 def test_set_motor_speed1():
-    driver = Driver("/dev/tty.usbserial-2130", report=True)
-    driver.set_motor_speed(2, 600)
-
-    time.sleep(0.1)
-
-    msg = driver.get_latest_message()
+    driver = SerialDriver("/dev/tty.usbserial-2130", report=True)
+    msg = driver.set_motor_speed(2, 600)
 
     assert msg == "OK!"
 
     log.info(msg)
+
+    driver.close()
 
 
 def test_motor_stop():
-    driver = Driver("/dev/tty.usbserial-2130", report=True)
-    driver.motor_stop(2)
-
-    time.sleep(0.5)
-
-    msg = driver.get_latest_message()
+    driver = SerialDriver("/dev/tty.usbserial-2130", report=True)
+    msg = driver.motor_stop(2)
 
     assert msg == "OK!"
 
     log.info(msg)
+
+    driver.close()
 
 
 def test_move_serial_servo1():
-    driver = Driver("/dev/tty.usbserial-2130", report=True)
-    driver.move_serial_servo(1, 2000, 500)
-
-    time.sleep(0.1)
-
-    msg = driver.get_latest_message()
+    driver = SerialDriver("/dev/tty.usbserial-2130", report=True)
+    msg = driver.move_serial_servo(1, 2000, 500)
 
     assert msg == "OK!"
 
     log.info(msg)
+
+    driver.close()
 
 
 def test_move_serial_servo2():
-    driver = Driver("/dev/tty.usbserial-2130", report=True)
-    driver.move_serial_servo(1, 3999, 500)
-
-    time.sleep(0.1)
-
-    msg = driver.get_latest_message()
+    driver = SerialDriver("/dev/tty.usbserial-2130", report=True)
+    msg = driver.move_serial_servo(1, 3999, 500)
 
     assert msg == "OK!"
 
     log.info(msg)
 
+    driver.close()
+
 
 def test_get_serial_servo_angle():
-    driver = Driver("/dev/tty.usbserial-2130", report=True)
+    driver = SerialDriver("/dev/tty.usbserial-2130", report=True)
     driver.get_serial_servo_angle(1)
 
     time.sleep(0.1)
 
-    msg = driver.get_latest_message()
+    # msg = driver.get_latest_message()
 
-    log.info(msg)
+    # log.info(msg)
 
 
 def test_get_get_encoder_values():
-    driver = Driver("/dev/tty.usbserial-2130", report=True)
-    driver.get_encoder_values()
+    driver = SerialDriver("/dev/tty.usbserial-2130", report=True)
+    msg = driver.get_encoder_values()
 
-    time.sleep(0.1)
-
-    rpms = driver.get_latest_rpm()
-    log.info(rpms)
+    log.info(msg)
